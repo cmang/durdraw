d02c8f870c511b4ff00d9144cadab4d9e26f41d4
Sam Foster <samfoster@gmail.com>
2023-12-15 06:08:07 -0600

    0.23.1 version update, README change on editing 16-color PC ansi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f_readme:
 
 setup(
     name='durdraw',
-    version='0.23.0',
+    version='0.23.1',
     author='Sam Foster',
     author_email='samfoster@gmail.com',
     description='Animated Color ASCII and Unicode Art Editor',
