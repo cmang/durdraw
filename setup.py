17519933ecefd08846b83206ab7249cec7c0ad4a
Sam Foster <samfoster@gmail.com>
2024-10-19 10:38:13 -0500

    Version bump to 0.28.0 for release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f_readme:
 
 setup(
     name='durdraw',
-    version='0.28.0-BETA',
+    version='0.28.0',
     author='Sam Foster',
     author_email='samfoster@gmail.com',
     description='Animated Color ASCII and Unicode Art Editor',
