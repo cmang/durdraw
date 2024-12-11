64d20c069557dc49724a00e960053b6d11cc0bbf
Tom McKeesick <tmck01@gmail.com>
2024-12-12 09:13:37 +1100

    test docstrings and complex states

--- test/durdraw/test_undo.py
+++ test/durdraw/test_undo.py
@@ -1,10 +1,12 @@
-from collections import deque
+from collections import deque, namedtuple
 
 import durdraw.durdraw_undo as undo
 
 class TestUndo:
 
     def test_push(self):
+        'initial state is set, push adds new state to the undo buffer'
+
         r = undo.UndoRegister(1)
         assert r.state == 1
 
@@ -13,6 +15,8 @@ class TestUndo:
         assert r.undoBuf, r.redoBuf == (deque([1,2]), deque())
 
     def test_undo(self):
+        'undo returns the previous state, and removes it from the undo buffer'
+
         r = undo.UndoRegister(1)
         r.push(2)
         r.push(3)
@@ -23,6 +27,8 @@ class TestUndo:
         assert r.undoBuf, r.redoBuf == (deque([1,2]), deque([3]))
 
     def test_undo_no_history(self):
+        'undo returns None if there is no history to undo'
+
         r = undo.UndoRegister(1)
         assert r.state == 1
 
@@ -44,6 +50,8 @@ class TestUndo:
         assert r.undo() == None
 
     def test_redo(self):
+        'redo returns the next state, and removes it from the redo buffer'
+
         r = undo.UndoRegister(1) # initial state, cannot be undone
         r.push(2) # 1st change
         r.push(3) # 2nd change
@@ -67,6 +75,8 @@ class TestUndo:
         assert r.undoBuf, r.redoBuf == (deque([1,2,3]), deque())
 
     def test_redo_no_history(self):
+        'redo returns None if there is no history to redo'
+
         r = undo.UndoRegister(1)
         r.push(2)
 
@@ -87,6 +97,8 @@ class TestUndo:
         assert r.redo() == None
 
     def test_redo_push(self):
+        'redo buffer is cleared when a new state is pushed after undo'
+
         r = undo.UndoRegister(1)
         r.push(2)
         r.push(3)
@@ -109,3 +121,19 @@ class TestUndo:
         assert r.redo() == None
         assert r.state == 4
         assert r.undoBuf, r.redoBuf == (deque([1,4]), deque())
+
+    def test_complex_states(self):
+        'undo and redo with more complex states'
+        state = namedtuple('state', 'a b c')
+        s0, s1, s2 = state(0,0,0), state(1,2,3), state(4,5,6)
+
+        r = undo.UndoRegister(s1)
+        r.push(s1)
+
+        assert r.state == s1
+        assert r.undoBuf, r.redoBuf == (deque([s0, s1]), deque())
+
+        r.push(s2)
+        assert r.state == s2
+        assert r.undoBuf, r.redoBuf == (deque([s0, s1, s2]), deque())
+
